52318bb93d62849d97f435433b7907de1989ec00
3rdid <erickson.jon@3rdinf.us>
2015-06-26 19:49:08 -0700

    Fixed settings so they dont get overwritten with upgrade

--- upload/admin/applications_addon/other/perscom/setup/versions/install/knownSettings.php
+++ upload/admin/applications_addon/other/perscom/setup/versions/install/knownSettings.php
@@ -11,36 +11,4 @@
 
 $knownSettings = array(
 
-	// Database Settings
-	'perscom_database_personnel_files'		=> 'perscom_personnel',
-	'perscom_database_ranks'				=> 'perscom_ranks',
-	'perscom_database_awards'				=> 'perscom_awards',
-	'perscom_database_units'				=> 'perscom_combat_units',
-	'perscom_database_weapons'				=> 'perscom_weapons',
-	'perscom_database_dmos'					=> 'perscom_admin_units',
-	'perscom_database_status'				=> 'perscom_status',
-	'perscom_database_records'				=> 'perscom_service_records',
-	'perscom_database_tpr'					=> 'perscom_temporary_pass_requests',
-	'perscom_database_loa'					=> 'perscom_leave_of_absences',
-	'perscom_database_muster'				=> 'perscom_muster_logs',
-	'perscom_database_requests'				=> 'perscom_requests',
-	'perscom_database_settings'				=> 'perscom_settings',
-
-	// General Settings
-	'perscom_disable'						=> '0',
-
-	// Usergroup Settings
-	'perscom_base_unit_usergroup'			=> '4',
-	'perscom_administrative_usergroup'		=> '4',
-	'perscom_bmo_usergroup'					=> '4',
-	'perscom_civilian_usergroup'			=> '3',
-	'perscom_retired_usergroup'				=> '4',
-	'perscom_application_submission_group'	=> '3',
-
-	// Miscellaneous Settings
-	'perscom_tpr_expiration'				=> '3',
-	'perscom_disable_avatar'				=> '1',
-	'perscom_enable_muster'					=> '1',
-	'perscom_enable_profile_pfile'			=> '1',
-	'perscom_enable_steam'					=> '1',
 );
